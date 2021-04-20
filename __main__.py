06831bdcf4aed51735420779dfc41beea5003d51
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2021-04-20 11:47:50 +0200

    Update __main__.py
    Removal of unnecessary header

--- __main__.py
+++ __main__.py
@@ -1,23 +1,6 @@
 import requests
 import sys
 
-headers = {
-    'Connection': 'keep-alive',
-    'Accept': 'application/json',
-    'Cache-Control': 'no-cache',
-    'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-    'Sec-GPC': '1',
-    'Origin': 'https://lot.moe',
-    'Sec-Fetch-Site': 'same-site',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': 'https://lot.moe/',
-    'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-    'dnt': '1',
-}
-
-
 
 class UploadAPI:
     def __init__(self, api_url, file_path):
@@ -35,7 +18,7 @@ class UploadAPI:
         return return_data
 
     def make_request(self):
-        response = requests.post(self.api_url, headers=headers, files=self.get_data())
+        response = requests.post(self.api_url, files=self.get_data())
         return response.text
 
 
